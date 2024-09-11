b96491464fa3669476efb1a5abf73d1c98e515c3
rafa <rafaelrgo.icm@gmail.com>
2024-09-11 11:29:56 -0300

    Delete TelaDeLoginTest.java

--- TelaDeLoginTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class TelaDeLoginTest extends JFrame
-{
-    public static void main(String[] args) {
-        TelaDeLogin appTelaDeLogin = new TelaDeLogin();
-        appTelaDeLogin.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-}
